907389cac905fc5f0e397190732c04c1479c6ff1
sigmachad <xander515@tampabay.rr.com>
2026-06-16 10:38:17 -0400

    fix vscode install script

--- install_code.sh
+++ install_code.sh
@@ -1,13 +1,14 @@
 # Update system packages and install dependencies
-sudo apt update
-sudo apt install wget gpg apt-transport-https
-
-# Download and import the Microsoft GPG signing key
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/packages.microsoft.gpg > /dev/null
+sudo apt install wget gpg &&
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft.gpg
 
 # Add the official VS Code repository to your sources
-echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://microsoft.com stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+echo "Types: deb
+URIs: https://packages.microsoft.com/repos/code
+Suites: stable
+Components: main
+Architectures: amd64,arm64,armhf
+Signed-By: /usr/share/keyrings/microsoft.gpg" | sudo tee /etc/apt/sources.list.d/vscode.sources
 
 # Update package index and install VS Code
-sudo apt update
-sudo apt install code
+sudo apt update && sudo apt install code
